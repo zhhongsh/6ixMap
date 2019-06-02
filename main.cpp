4909ff5d00b45384208676ed532a6fe4695df0d8
KrishnaSolo <39069859+KrishnaSolo@users.noreply.github.com>
2019-06-02 19:20:43 -0400

    added main

---
+++ main.cpp
@@ -0,0 +1,81 @@
+/* 
+ * Copyright 2019 University of Toronto
+ *
+ * Permission is hereby granted, to use this software and associated 
+ * documentation files (the "Software") in course work at the University 
+ * of Toronto, or for personal use. Other uses are prohibited, in 
+ * particular the distribution of the Software either publicly or to third 
+ * parties.
+ *
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include "m1.h"
+#include "m2.h"
+#include "LatLon.h"
+#include "StreetsDatabaseAPI.h"
+#include "ezgl/application.hpp"
+#include "ezgl/graphics.hpp" 
+#include "math.h" 
+#include "m3.h"
+#include <ezgl/helpers.h>
+#include "class.h"
+#pragma once 
+
+
+//Program exit codes
+constexpr int SUCCESS_EXIT_CODE = 0;        //Everyting went OK
+constexpr int ERROR_EXIT_CODE = 1;          //An error occured
+constexpr int BAD_ARGUMENTS_EXIT_CODE = 2;  //Invalid command-line usage
+
+//The default map to load if none is specified
+std::string default_map_path = "/cad2/ece297s/public/maps/toronto_canada.streets.bin";
+
+
+int main(int argc, char** argv) {
+
+    std::string map_path;
+    if(argc == 1) {
+        //Use a default map
+        map_path = default_map_path;
+    } else if (argc == 2) {
+        //Get the map from the command line
+        map_path = argv[1];
+    } else {
+        //Invalid arguments
+        std::cerr << "Usage: " << argv[0] << " [map_file_path]\n";
+        std::cerr << "  If no map_file_path is provided a default map is loaded.\n";
+        return BAD_ARGUMENTS_EXIT_CODE;
+    }
+
+    //Load the map and related data structures
+    bool load_success = load_map(map_path);
+    if(!load_success) {
+        std::cerr << "Failed to load map '" << map_path << "'\n";
+        return ERROR_EXIT_CODE;
+    }
+
+    std::cout << "Successfully loaded map '" << map_path << "'\n";
+    
+    //You can now do something with the map data
+    draw_map();
+
+    //Clean-up the map data and related data structures
+    std::cout << "Closing map\n";
+    close_map(); 
+
+    return SUCCESS_EXIT_CODE;
+}
